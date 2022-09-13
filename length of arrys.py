0cdbe5ec84c4255564bd45baa6c6d32169a9a74d
SaiVvv <112847901+SaiVvv@users.noreply.github.com>
2022-09-13 09:06:16 +0530

    Add files via upload

---
+++ length of arrys.py
@@ -0,0 +1,11 @@
+b=int(input("Enter length of array"))
+a=[]
+e=[]
+for i in range(0,b):
+    c=input("Enter a string")
+    a.append(c)
+for i in range(0,b):
+    d=len(a[i].split())
+    e.append(d)
+print("list=",a)
+print("MAX words in a string=",MAX(E))
